b15556cb100115ff394903faa03f4379b67e3625
Κοσμάς Δήμας <dimaskosmas@outlook.com.gr>
2017-11-20 17:03:40 +0200

    eed some

--- Server/AndromedaNodeServer/models/Arc.js
+++ Server/AndromedaNodeServer/models/Arc.js
@@ -2,7 +2,7 @@
 
 module.exports = (sequelize, DataTypes) => {
     var Arc = sequelize.define('Arc', {
-        Id:         {type: sequelize.INTEGER(), allowNull:false                  },
+        Id:         {type: sequelize.INTEGER(),allowNull:false                  },
         Name:       {type: sequelize.STRING(), allowNull: false                  },
         Address:    {type: sequelize.STRING(), allowNull: false                  },
         Licence:    {type: sequelize.STRING(), allowNull: false, primaryKey: true}   
---
+++ Server/AndromedaNodeServer/seeders/20171120134811-InitialArcs.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        /*
+          Add altering commands here.
+          Return a promise to correctly handle asynchronicity.
+    
+          Example:
+          return queryInterface.bulkInsert('Person', [{
+            name: 'John Doe',
+            isBetaMember: false
+          }], {});
+        */
+        return queryInterface.bulkInsert('Arc', [{
+            Id: 0,
+            Name: 'Terra',
+            Address: 'etninkhs antistaeos 79',
+            Licence: '0000-2222-3333-4444'
+        },
+        {
+            Id: 0,
+            Name: 'Plan',
+            Address: 'etninkhs antistaeos 89',
+            Licence: '0000-2222-3333-5555'
+        },
+        {
+            Id: 0,
+            Name: 'Andomeda',
+            Address: 'etninkhs antistaeos 79',
+            Licence: '0000-2222-3333-6666'
+        },
+        {
+            Id: 0,
+            Name: 'kerver',
+            Address: 'etninkhs antistaeos 79',
+            Licence: '0000-2222-3333-7777'
+        },
+        {
+            Id: 0,
+            Name: 'Aoua',
+            Address: 'etninkhs antistaeos 79',
+            Licence: '0000-2222-3333-8888'
+        },
+        {
+            Id: 0,
+            Name: 'Avava',
+            Address: 'etninkhs antistaeos 79',
+            Licence: '0000-2222-3333-9999'
+        }], {}
+        );
+    },
+
+    down: (queryInterface, Sequelize) => {
+          /*
+            Add reverting commands here.
+            Return a promise to correctly handle asynchronicity.
+      
+            Example:
+            return queryInterface.bulkDelete('Person', null, {});
+          */
+        return queryInterface.bulkDelete('Arc',{ Licence: '0000-2222-3333-4444' });
+ 
+
+    }
+};
